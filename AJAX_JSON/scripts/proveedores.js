6da5d2d88450439fd882458667037da05af638fd
CarlitosNea <cardarober@gmail.com>
2023-06-13 20:09:16 -0500

    actualizacion front-end Proveedores

--- AJAX_JSON/scripts/proveedores.js
+++ AJAX_JSON/scripts/proveedores.js
@@ -8,12 +8,16 @@ $(document).ready(function(){
             success: function(res){
                 console.log(res);
                 tabla.empty(); // Limpiamos la tabla antes de agregar los datos
-                
+                //encabezado
+                let encabezado = $('<tr></tr>');
+                encabezado.append('<th>Nombre proveedor</th>');
+                encabezado.append('<th>Telefono</th>');
+                tabla.append(encabezado);
                 // Agregamos los datos a cada fila de la tabla
                 for (let i = 0; i < res.length; i++) {
                     let fila = $('<tr></tr>');
-                    fila.append('<th scope="row">' + "Nombre proveedor:" + '</th>');
                     fila.append('<td>' + res[i].nombre_proveedor + '</td>');
+                    fila.append('<td>' + res[i].telefono_proveedor + '</td>');
                     tabla.append(fila);
                 }
             }
@@ -35,38 +39,51 @@ $(document).ready(function(){
         });
     });
 
-    $('#enviar').on('click', function(){
+    $('#enviar').on('click', function() {
         let datos = {
             nombre_proveedor: $('#nombre_input_novo').val(),
-        }
-        let datosEnvio = JSON.stringify(datos)
-        console.log(datos)
-        console.log(datosEnvio)
+            telefono_proveedor: $('#telefono_input_novo').val(),
+        };
+        let datosEnvio = JSON.stringify(datos);
+        console.log(datos);
+        console.log(datosEnvio);
         $.ajax({
             url: "http://localhost:8080/insertarProveedor",
             type: "POST",
             data: datosEnvio,
-            contentType: "application/JSON",
-            datatype: JSON,
-            success: function(respuesta){
-                alert(respuesta)
+            contentType: "application/json", // Corregido: el tipo de contenido debe ser "application/json"
+            dataType: "json", // Corregido: dataType en minúscula
+            success: function(respuesta) {
+                if (respuesta) {
+                    alert("Registrado exitosamente");
+                } else {
+                    alert("error");
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
             }
-        })
-    })
+        });
+    });
 
 
-    $('#eliminar').on('click', function(){
+    $('#eliminar').on('click', function() {
         let dato = $('#nombre_input_delete').val();
         $.ajax({
-            url: "http://localhost:8080/eliminarProveedor/"+dato,
+            url: "http://localhost:8080/eliminarProveedor/" + dato,
             type: "DELETE",
-            dataType: "JSON",
-            success: function(respuesta){
-                if(respuesta)
-                    alert("eliminado exitosamente")
-                else
-                    alert("no existe el dato")
-            }})
-        })
+            dataType: "json", // Corregido: dataType en minúscula
+                success: function(respuesta) {
+                    if (respuesta) {
+                        alert("Eliminado exitosamente");
+                    } else {
+                        alert("No existe el dato");
+                    }
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log(textStatus, errorThrown);
+                }
+        });
+    });
 
 })
